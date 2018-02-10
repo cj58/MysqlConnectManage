81527de09d4dc2ebc9b47b051e1fb4d11bd6456b
cj <30950295+cj58@users.noreply.github.com>
2018-02-10 16:30:52 +0800

    Update mysql_connect_core_func.sh
    add info

--- mysql_connect_core_func.sh
+++ mysql_connect_core_func.sh
@@ -1,62 +1,62 @@
 #!/bin/sh
 # mysql_connect_core_func.sh
 # 
-# 连接配置核心函数
+# mysql connect core func
 #
 # @link https://github.com/cj58/MysqlConnectManage
 # @author cj
 # @copyright 2018 cj
 #
 
-##! @TODO: 打印帮助文档
+##! @TODO: Help document
 ##! @OUT: 0 => success; 1 => failure 
 function printHelp()
 {
     echoYellow '===============================================================================';
-    echoYellow '# 帮助文档：https://github.com/cj58/MysqlConnectManage #';
+    echoYellow '# Help Document：https://github.com/cj58/MysqlConnectManage #';
     echoYellow '===============================================================================';
     echoYellow '';
 }
 
-##! @TODO: 连接数据库
+##! @TODO: doMysqlConnect
 ##! @IN: $1 => connectStr
 ##! @OUT: 0 => success; 1 => failure 
 function doMysqlConnect()
 {
     local connectStr=$1
     if [ -z "${connectStr}" ];then
-        echoRed '[ERROR],connectStr 参数不能为空! in doMysqlConnect()'
+        echoRed '[ERROR],connectStr config can not be empty! in doMysqlConnect()'
         return 1;
     fi 
 
-    echoGreen "[INFO] ${connectStr} 连接中..."
+    echoGreen "[INFO] ${connectStr} connecting..."
     ${connectStr}
     return 0;
 }
 
-##! @TODO: 选择连接方式
+##! @TODO: choiceConnect
 ##! @IN: $1 => configFile
-##! @IN: $2 => choiceNum 传递的选择数字
+##! @IN: $2 => choiceNum 
 ##! @OUT: 0 => success; 1 => failure 
 function choiceConnect()
 {
     local configFile=$1
     if [ -z ${configFile} ];then
-        echoRed '[ERROR],configFile 参数不能为空! in addNewConnect()'
+        echoRed '[ERROR],configFile config can not be empty! in addNewConnect()'
         return 1;
     fi 
 
-    #当配置文件不存在的时候，添加一个新的连接
+    #config file not exits
     if [ ! -f ${configFile} ];then
         addNewConnect "${configFile}" 
         return 0;
     fi
 
-    echo "请选择需要连接的数据库："
+    echo "Select the database you want to connect to:"
     local tmpNum=1;
     local choiceConnectHash;
-    choiceConnectHash[0]='手动添加新的数据库连接'
-    echo "0 手动添加新的数据库连接"
+    choiceConnectHash[0]='Add a new database connection'
+    echo "0 Add a new database connection"
     local line='';
 
     while read line
@@ -69,7 +69,7 @@ function choiceConnect()
     local choiceNum=0
     echo '';
     if [ ! $2 ];then
-        read -p "请输入连接方式对应的数字(例如：1)：" choiceNum
+        read -p "Please enter the number corresponding to the connection mode(example:1):" choiceNum
     else
         choiceNum=$2;
     fi
@@ -90,7 +90,7 @@ function choiceConnect()
     done
 
     if [ ${found} -eq 0 ];then
-        echoRed "[ERROR],选择的 ${choiceNum} 连接方式不存在,请重新选择"
+        echoRed "[ERROR],choice ${choiceNum} not exist,try again"
         exit;
     fi
 
@@ -98,14 +98,14 @@ function choiceConnect()
 }
 
 
-##! @TODO: 添加一个新的连接
+##! @TODO: add New Connect
 ##! @IN: $1 => configFile
 ##! @OUT: 0 => success; 1 => failure 
 function addNewConnect()
 {
     local configFile=$1
     if [ -z ${configFile} ];then
-        echoRed '[ERROR],configFile 参数不能为空! in addNewConnect()'
+        echoRed '[ERROR],configFile  can not be empty! in addNewConnect()'
         return 1;
     fi 
 
@@ -115,23 +115,23 @@ function addNewConnect()
     local password='';
 
     echo '';
-    read -p "  请输入主机名或IP地址（例如：weihu1-db.bj1.xxx.net）：" host
-    read -p "  请输入端口(例如：3306)：" port
-    read -p "  请输入用户名(例如：root)：" user
-    read -p "  请输入密码(例如：123456)：" password
+    read -p "  database Host or IP(example:weihu1-db.bj1.xxx.net):" host
+    read -p "  port(example:3306):" port
+    read -p "  user(example:root):" user
+    read -p "  password(example:123456):" password
     host=$(trimStr "${host}")
     port=$(trimStr "${port}")
     user=$(trimStr "${user}")
     password=$(trimStr "${password}")
     if [[ -z ${host} || -z ${port} || -z ${user} || -z ${password} ]];then
-        echoRed "[ERROR],全部输入都不能为空！";
+        echoRed "[ERROR],All input cannot be empty!";
         exit;
     fi
 
     local connectStr='mysql -h '${host}' -P '${port}' -u'${user}' -p'${password}
 
     echo ${connectStr} >> ${configFile} 
-    echoGreen "[INFO] 添加 ${connectStr} 完成"
+    echoGreen "[INFO] add ${connectStr} complete"
 
     doMysqlConnect "${connectStr}"
     return 0;
